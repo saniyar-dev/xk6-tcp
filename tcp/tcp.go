e924f14fb8ee1052191938f50905a7322e6909ae
saniyar <saniyar.karami@arvancloud.ir>
2025-01-28 20:10:17 +0330

    [ ADD ] structure of TCPAPI and Root Module

--- tcp/tcp.go
+++ tcp/tcp.go
@@ -1,1 +1,38 @@
-package xk6tcp
+package tcp
+
+import (
+	"github.com/grafana/sobek"
+	"go.k6.io/k6/js/modules"
+)
+
+type RootModule struct{}
+
+var _ modules.Module = &RootModule{}
+
+func (r *RootModule) NewModuleInstance(vu modules.VU) modules.Instance {
+	return &TCPAPI{
+		vu: vu,
+	}
+}
+
+type TCPAPI struct {
+	vu modules.VU
+	// blobConstructor sobek.Value
+}
+
+var _ modules.Instance = &TCPAPI{}
+
+// Exports implements the modules.Instance interface's Exports
+func (r *TCPAPI) Exports() modules.Exports {
+	// r.blobConstructor = r.vu.Runtime().ToValue(r.blob)
+	return modules.Exports{
+		Named: map[string]interface{}{
+			"TCP": r.tcp,
+			// "Blob": r.blobConstructor,
+		},
+	}
+}
+
+func (r *TCPAPI) tcp(c sobek.ConstructorCall) *sobek.Object {
+	return &sobek.Object{}
+}
